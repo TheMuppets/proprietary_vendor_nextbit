d4aadc42d52acb72c8111719b1b3ad40d9a1cf2d
chrmhoffmann <chrmhoffmann@gmail.com>
2018-05-11 14:51:28 +0200

    ether: Move blobs to vendor
    * Requires following hex edit to load configs from vendor:
    sed -i "s|/system/etc/nxp|/vendor/etc/nxp|g" vendor/lib64/libFIHNxp.so
    sed -i "s|/system/etc/nxp|/vendor/etc/nxp|g" vendor/lib/libFIHNxp.so
    sed -i "s|/system/etc/data/dsi_config.xml|/vendor/etc/data/dsi_config.xml|g" vendor/lib64/libdsi_netctrl.so
    sed -i "s|/system/etc/data/dsi_config.xml|/vendor/etc/data/dsi_config.xml|g" vendor/lib/libdsi_netctrl.so
    sed -i "s|/system/etc/data/netmgr_config.xml|/vendor/etc/data/netmgr_config.xml|g" vendor/bin/netmgrd
    sed -i "s|/system/etc/data/qmi_config.xml|/vendor/etc/data/qmi_config.xml|g" vendor/bin/qmuxd
    sed -i "s|/system/etc/|/vendor/etc/|g" vendor/bin/thermal-engine
    
    Change-Id: Ia899dd04b7b06ba25e0d5e72d2a07cc18895ba14

--- ether/Android.mk
+++ ether/Android.mk
@@ -22,32 +22,35 @@ ifeq ($(TARGET_DEVICE),ether)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libloc_api_v02
 LOCAL_MODULE_OWNER := nextbit
-LOCAL_SRC_FILES := proprietary/lib64/libloc_api_v02.so
+LOCAL_SRC_FILES := proprietary/vendor/lib64/libloc_api_v02.so
 LOCAL_MULTILIB := 64
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libloc_ds_api
 LOCAL_MODULE_OWNER := nextbit
-LOCAL_SRC_FILES := proprietary/lib64/libloc_ds_api.so
+LOCAL_SRC_FILES := proprietary/vendor/lib64/libloc_ds_api.so
 LOCAL_MULTILIB := 64
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := TimeService
 LOCAL_MODULE_OWNER := nextbit
-LOCAL_SRC_FILES := proprietary/app/TimeService/TimeService.apk
+LOCAL_SRC_FILES := proprietary/vendor/app/TimeService/TimeService.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
 LOCAL_MODULE_SUFFIX := .apk
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 endif
